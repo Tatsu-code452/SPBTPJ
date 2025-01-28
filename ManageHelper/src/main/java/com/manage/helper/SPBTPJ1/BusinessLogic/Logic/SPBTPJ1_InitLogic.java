d3fc5fc4eb4393c765419d880abe81283ec8f4d9
t.ohkura <tatsumi.ookura@kkmiura.jp>
2025-01-28 12:33:00 +0900

    ソート処理追加

--- ManageHelper/src/main/java/com/manage/helper/SPBTPJ1/BusinessLogic/Logic/SPBTPJ1_InitLogic.java
+++ ManageHelper/src/main/java/com/manage/helper/SPBTPJ1/BusinessLogic/Logic/SPBTPJ1_InitLogic.java
@@ -1,8 +1,10 @@
 package com.manage.helper.SPBTPJ1.BusinessLogic.Logic;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
@@ -19,31 +21,37 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class SPBTPJ1_InitLogic extends BaseLogic<SPBTPJ1_InitBDto> {
-    private final FilePathDao filePathDao;
-    private final FilePathGroupDao filePathGroupDao;
+	private final FilePathDao filePathDao;
+	private final FilePathGroupDao filePathGroupDao;
 
-    private List<FilePathDto> filePathList;
-    private List<FilePathGroupDto> filePathGroupList;
+	private List<FilePathDto> filePathList;
+	private List<FilePathGroupDto> filePathGroupList;
 
-    @Override
-    protected boolean loadData(SPBTPJ1_InitBDto dto) {
-        boolean result = true;
-        filePathGroupList = filePathGroupDao.readAll();
-        filePathList = filePathDao.readAll();
-        return result;
-    }
+	@Override
+	protected boolean loadData(SPBTPJ1_InitBDto dto) {
+		boolean result = true;
+		filePathGroupList = filePathGroupDao.readAll();
+		filePathList = filePathDao.readAll();
+		return result;
+	}
 
-    @Override
-    protected boolean createResponse(SPBTPJ1_InitBDto dto) {
-        boolean result = true;
-        dto.getViewModel()
-                .setFilePathGroupMap(filePathGroupList.stream()
-                        .sorted(Comparator.comparing(FilePathGroupDto::getOrder,
-                                Comparator.nullsLast(Comparator.naturalOrder())))
-                        .collect(Collectors.toMap(FilePathGroupDto::getGroupId,
-                                FilePathGroupDto::getGroup, (e1, e2) -> e1,
-                                LinkedHashMap::new)));
-        dto.getViewModel().setFilePathList(filePathList);
-        return result;
-    }
+	@Override
+	protected boolean createResponse(SPBTPJ1_InitBDto dto) {
+		boolean result = true;
+		dto.getViewModel()
+				.setFilePathGroupMap(filePathGroupList.stream()
+						.sorted(Comparator.comparing(FilePathGroupDto::getOrder,
+								Comparator.nullsLast(Comparator.naturalOrder())))
+						.collect(Collectors.toMap(FilePathGroupDto::getGroupId, FilePathGroupDto::getGroup,
+								(e1, e2) -> e1, LinkedHashMap::new)));
+
+		List<String> orderedGroupIdList = dto.getViewModel().getFilePathGroupMap().keySet().stream()
+				.collect(Collectors.toList());
+		dto.getViewModel().setFilePathList(
+				filePathList.stream().sorted((n1, n2) -> Integer.compare(orderedGroupIdList.indexOf(n1.getGroupId()),
+						orderedGroupIdList.indexOf(n2.getGroupId()))).collect(Collectors.toList()));
+
+		return result;
+	}
+
 }
