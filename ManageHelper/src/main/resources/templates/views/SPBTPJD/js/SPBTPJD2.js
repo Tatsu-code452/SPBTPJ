61e6311ebc674f65f88c8a84d32249e0006af30e
Tatsu-code452 <tatsu.dragon.416@gmail.com>
2025-03-12 17:39:04 +0900

    CSV分割機能整理

--- ManageHelper/src/main/resources/templates/views/SPBTPJD/js/SPBTPJD2.js
+++ ManageHelper/src/main/resources/templates/views/SPBTPJD/js/SPBTPJD2.js
@@ -6,15 +6,70 @@ document.addEventListener("DOMContentLoaded", function () {
     // CSV読み込み
     loadCsv("./data/parse.csv", parseCSV);
 
-    const textArea = document.getElementById("textArea");
+    const textArea = document.querySelector("#textArea");
     // テキストエリア入力設定
-    textArea.addEventListener("input", () => setInputTextArea(textArea));
+    textArea.addEventListener("input", () => splitTextArea(textArea));
 });
 
-// テキストエリア入力設定
-function setInputTextArea(textArea) {
+// CSV解析
+function parseCSV(text) {
+    const table = document.createElement("table");
+    table.id = "parsedTable";
+    table.appendChild(createHeader());
+    table.appendChild(createBody(text));
+    const tableWrapper = document.querySelector("#tableWrapper");
+    tableWrapper.replaceChildren(table);
+
+    const textArea = document.querySelector("#textArea");
+    if (textArea.value) {
+        splitTextArea(textArea);
+    }
+}
+
+// ヘッダー行作成
+function createHeader() {
+    // ヘッダー行を追加
+    const tableHeader = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+    const headers = ["項目名", "桁数"];
+    headers.forEach((header) => {
+        const th = document.createElement("th");
+        th.textContent = header;
+        headerRow.appendChild(th);
+    });
+    tableHeader.appendChild(headerRow);
+    return tableHeader;
+}
+
+// ボディー行作成
+function createBody(text) {
+    const tableBody = document.createElement("tbody");
+    const rows = text.split("\r\n");
+    rows.forEach((row) => {
+        const cols = row.split(",");
+        const tr = document.createElement("tr");
+        cols.forEach((col) => {
+            const td = document.createElement("td");
+            td.textContent = col;
+            tr.appendChild(td);
+        });
+        tableBody.appendChild(tr);
+    });
+    return tableBody;
+}
+
+// テキストエリア分割
+function splitTextArea(textArea) {
     const textRows = textArea.value.split("\n");
     const parsedTable = document.querySelector("#parsedTable");
+    const headerRow = parsedTable.rows[0];
+    textRows.forEach((text, textRowIndex) => {
+        if (headerRow.cells.length <= textRowIndex + 2) {
+            const th = document.createElement("th");
+            th.textContent = parseInt(textRowIndex) + 1 + "行目";
+            headerRow.appendChild(th);
+        }
+    });
 
     let beforePos = 0;
     for (let rowIndex = 0; rowIndex < parsedTable.rows.length; rowIndex++) {
@@ -37,35 +92,21 @@ function setInputTextArea(textArea) {
 
 // テキスト分割
 function splitText(tableRow, text, textRowIndex, startPos, endPos) {
-    if (text.length >= endPos) {
-        if (tableRow.cells.length <= textRowIndex + 2) {
-            const td = document.createElement("td");
-            td.textContent = text.slice(startPos, endPos);
-            tableRow.appendChild(td);
-        } else {
-            tableRow.cells[textRowIndex + 2].textContent = text.slice(
-                startPos,
-                endPos
-            );
-        }
-    }
+    const cellIndex = textRowIndex + 2;
+    const slicedText = text.slice(startPos, endPos);
+    const isError = text.length < endPos;
+
+    updateTableCell(tableRow, cellIndex, slicedText, isError);
 }
 
-// CSV表示
-function parseCSV(text) {
-    const rows = text.split("\r\n");
-    const table = document.createElement("table");
-    table.id = "parsedTable";
-    rows.forEach((row) => {
-        const cols = row.split(",");
-        const tr = document.createElement("tr");
-        cols.forEach((col) => {
-            const td = document.createElement("td");
-            td.textContent = col;
-            tr.appendChild(td);
-        });
-        table.appendChild(tr);
-    });
-    const parseWrapper = document.querySelector("#parseWrapper");
-    parseWrapper.replaceChildren(table);
+// テーブルセル更新
+function updateTableCell(tableRow, cellIndex, text, isError) {
+    if (tableRow.cells.length <= cellIndex) {
+        const td = document.createElement("td");
+        td.textContent = text;
+        tableRow.appendChild(td);
+    } else {
+        tableRow.cells[cellIndex].textContent = text;
+    }
+    tableRow.cells[cellIndex].classList.toggle("error", isError);
 }
