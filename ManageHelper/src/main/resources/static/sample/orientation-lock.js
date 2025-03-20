2a348717333a797c593466686c9e0d5bf997c7dc
Tatsu-code452 <tatsu.dragon.416@gmail.com>
2025-03-20 15:43:12 +0900

    整理

--- ManageHelper/src/main/resources/static/sample/orientation-lock.js
+++ ManageHelper/src/main/resources/static/sample/orientation-lock.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-	function lockOrientation() {
-		if (!window.screen.orientation) {
-			updateOrientationClass();
-		}
-	}
-
-	function updateOrientationClass() {
-		const isPortrait = window.innerHeight > window.innerWidth;
-		document.body.classList.toggle("rotate-portrait", isPortrait);
-		document.body.classList.toggle("rotate-landscape", !isPortrait);
-	}
-
-	lockOrientation();
-	updateOrientationClass();
-	window.addEventListener("resize", updateOrientationClass);
+    lockOrientation = () => {
+        if (!window.screen.orientation) {
+            const isPortrait = window.innerHeight > window.innerWidth;
+            document.body.classList.toggle("rotate-portrait", isPortrait);
+            document.body.classList.toggle("rotate-landscape", !isPortrait);
+        }
+    };
+    updateOrientationClass();
+    window.addEventListener("resize", updateOrientationClass);
 });
+
+function updateOrientationClass() {
+    const isPortrait = window.innerHeight > window.innerWidth;
+    document.body.classList.toggle("rotate-portrait", isPortrait);
+    document.body.classList.toggle("rotate-landscape", !isPortrait);
+}
